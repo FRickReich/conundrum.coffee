08321d7cd24c5f173c194684aa00e2e0dd836b99
FRickReich <frickreich@gmail.com>
2023-03-11 21:43:03 +0100

    Add user email showcase to dashboard component

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,30 +1,38 @@
+import { useEffect } from "react";
 import { useNavigate } from "react-router";
 import { useUserAuth } from "../../context/UserAuthContext";
 
-const Dashboard = () => {
-  const { logOut, user } = useUserAuth();
-  const navigate = useNavigate();
-  const handleLogout = async () => {
-    try {
-      await logOut();
-      navigate("/");
-    } catch (error) {
-      console.log(error.message);
-    }
-  };
+const Dashboard = () =>
+{
+    const { logOut, user } = useUserAuth();
+    const navigate = useNavigate();
 
-  return (
-    <>
-      <div>
-        Hello Welcome
-        <br />
-      </div>
-      <div>
-        <button onClick={handleLogout}>
-          Log out
-        </button>
-      </div>
-    </>
+    useEffect(() =>
+    {
+        console.log(user);
+    }, [user])
+    const handleLogout = async () =>
+    {
+        try{
+            await logOut();
+            navigate("/");
+        } catch (error) {
+            console.log(error.message);
+        }
+    };
+
+    return (
+        <>
+            <div>
+                Hello, { user?.email }
+                <br />
+            </div>
+            <div>
+                <button onClick={handleLogout}>
+                    Log out
+                </button>
+            </div>
+        </>
   );
 };
 
