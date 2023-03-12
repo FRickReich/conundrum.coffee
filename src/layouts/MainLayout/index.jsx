ba8d5beb055a1cb325524b894dbca529fe9685e8
FRickReich <frickreich@gmail.com>
2023-03-12 19:04:50 +0100

    Delete user auth from MainLayout component

--- src/layouts/MainLayout/index.jsx
+++ src/layouts/MainLayout/index.jsx
@@ -1,12 +1,9 @@
 import HeaderBar from "../../components/HeaderBar";
-import { useUserAuth } from "../../context/UserAuthContext";
 
 import './MainLayout.scss';
 
 const MainLayout = ({ children }) =>
 {
-    // const { user } = useUserAuth();
-
     return (
         <div className="MainLayout">
             <HeaderBar/>
@@ -19,14 +16,3 @@ const MainLayout = ({ children }) =>
 };
   
 export default MainLayout;
-
-{/* <div className="MainLayout__header">
-                <div className="MainLayout__header__logo">
-                    A
-                </div>
-                <div className="MainLayout__header__menu">
-                    <Button>Sign Up</Button>
-                    <Button>Log In</Button>
-                </div>
-                {/* header - { user ? 'logged in' : 'not logged in' } */}
-            // </div>
