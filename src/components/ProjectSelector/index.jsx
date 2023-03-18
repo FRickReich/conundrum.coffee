9adb7ee0422eda2300b82d24a8402f5b4c83ef07
FRickReich <frickreich@gmail.com>
2023-03-18 15:00:32 +0100

    Reimplement EditorLayout

--- src/components/ProjectSelector/index.jsx
+++ src/components/ProjectSelector/index.jsx
@@ -2,21 +2,24 @@ import { useEffect, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faChevronCircleDown, faCircleXmark } from '@fortawesome/free-solid-svg-icons';
 
-import { useOutsideClick, useProject } from './../../hooks';
+import { useOutsideClick } from './../../hooks';
 
 import Button from './../Button';
 
 import './ProjectSelector.scss';
 import Panel from '../Panel';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { collection, getDocs, query, where } from 'firebase/firestore';
 import { db } from '../../firebase';
-import { useUserAuth } from '../../context';
 
 const ProjectSelector = ({ userInfo, ...props }) => {
+
+    const location = useLocation();
+
     const [open, setOpen] = useState(false);
     const [projects, setProjects] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
+    const [projectByUser, setProjectByUser] = useState("");
 
     const handleClickOutside = () => {
         setOpen(false);
@@ -28,20 +31,46 @@ const ProjectSelector = ({ userInfo, ...props }) => {
         setOpen(!open);
     }
 
+    /**
+     * TODO: If a new project is created, the selector does not show the name, instead, it looks like no project was selected.
+     */
+    const handleCurrentProjectByOwner = async () =>
+    {
+        const currentPath = location.pathname.split(`/`);
+
+        const currentProjectUid = currentPath[2];
+
+        setProjectByUser(false);
+
+        if(projects.length !== 0)
+        {
+            const currentProjectIndex = projects.findIndex(project => currentProjectUid === project.data().uid);
+
+            if(currentProjectIndex !== -1)
+            {
+                setProjectByUser(projects[currentProjectIndex].data().title)
+            }
+        }
+    }
+
     const getProjectsByOwner = async () => {
         const q = query(collection(db, "projects"), where("ownerId", "==", userInfo.uid));
 
         const ownerProjects = await getDocs(q);
 
-        setProjects(ownerProjects.docs);
-        setIsLoading(false);
-
-        console.log(ownerProjects.docs)
+        await setProjects(ownerProjects.docs);
+        await handleCurrentProjectByOwner();
+        await setIsLoading(false);
     }
 
     useEffect(() => {
         userInfo.uid && getProjectsByOwner();
-    }, [userInfo])
+    }, [userInfo]);
+
+    useEffect(() =>
+    {
+        handleCurrentProjectByOwner();
+    }, [location.pathname, projects, open]);
 
     return (
         <div className="ProjectSelector" ref={myRef} >
@@ -53,6 +82,12 @@ const ProjectSelector = ({ userInfo, ...props }) => {
                     <div className="ProjectSelector__header__meta__username">
                         {userInfo.username}
                     </div>
+                    {
+                        projectByUser &&
+                        <div className="ProjectSelector__header__meta__project">
+                            { projectByUser }
+                        </div>
+                    }
                 </div>
                 <FontAwesomeIcon
                     className={`ProjectSelector__header__interaction ${open ? 'open' : ''}`}
@@ -72,7 +107,14 @@ const ProjectSelector = ({ userInfo, ...props }) => {
                                                 <ul>
                                                     {
                                                         projects.map((project, i) => {
-                                                            return <li key={i}><Link to={`/${userInfo.username}/${project.data().uid}`}>{project.data().title}</Link></li>
+                                                            return <li key={i}>
+                                                                <Link
+                                                                    to={`/${userInfo.username}/${project.data().uid}`}
+                                                                    onClick={() => setOpen(false)}
+                                                                >
+                                                                        {project.data().title}
+                                                                </Link>
+                                                            </li>
                                                         })
                                                     }
                                                 </ul>
@@ -84,7 +126,7 @@ const ProjectSelector = ({ userInfo, ...props }) => {
                                                 >
                                                     You have not yet created any projects.
                                                     <br />
-                                                    <Link to={`/${userInfo.username}/create-project`}>Create one now!</Link>
+                                                    <Link to={`/${userInfo.username}/create-project`} onClick={() => setOpen(false)}>Create one now!</Link>
                                                 </Panel>
 
                                             )
--- src/pages/Editor/index.jsx
+++ src/pages/Editor/index.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { useParams } from 'react-router';
+import EditorLayout from '../../layouts/EditorLayout';
 
 const Editor = () =>
 {
-        return(
-        <p>Hello World!</p>
+    const { username, project } = useParams();
+
+    return(
+        // <p>Hello World! - {username} { project }</p>
+            <EditorLayout></EditorLayout>
     );
 };
 
